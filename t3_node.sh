1619d4dab6d0c876d1721c73b1b4ed2d98057f4b
waves <8280998@qq.com>
2025-04-02 12:11:43 +0800

    Update t3_node.sh

--- t3_node.sh
+++ t3_node.sh
@@ -11,7 +11,7 @@ rm -f executor-linux-*.tar.gz
 curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | \
 grep -Po '"tag_name": "\K.*?(?=")' | \
 xargs -I {} wget https://github.com/t3rn/executor-release/releases/download/{}/executor-linux-{}.tar.gz
-pkill -f executor
+pkill -f ./executor
 sleep 5
 tar -xzf executor-linux-*.tar.gz
 export ENVIRONMENT=testnet
