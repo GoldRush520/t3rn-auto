70e3367eb1350f5b0cdb165b8088739d3dd4d00a
waves <8280998@qq.com>
2025-05-06 15:49:11 +0800

    Update t3_node.sh

--- t3_node.sh
+++ t3_node.sh
@@ -14,15 +14,25 @@ xargs -I {} wget https://github.com/t3rn/executor-release/releases/download/{}/e
 pkill -f ./executor
 sleep 5
 tar -xzf executor-linux-*.tar.gz
-export NODE_ENV=testnet
+export ENVIRONMENT=testnet
 export LOG_LEVEL=debug
 export LOG_PRETTY=false
 export EXECUTOR_MAX_L3_GAS_PRICE=1500
 export EXECUTOR_PROCESS_BIDS_ENABLED=true
 export EXECUTOR_PROCESS_ORDERS_ENABLED=true
 export EXECUTOR_PROCESS_CLAIMS_ENABLED=true
-export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn,unichain-sepolia'
-export RPC_ENDPOINTS_L1RN='https://brn.rpc.caldera.xyz/'
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,unichain-sepolia,blast-sepolia,l2rn'
+export RPC_ENDPOINTS='{
+    "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
+    "arbt": ["https://arbitrum-sepolia.drpc.org", "https://sepolia-rollup.arbitrum.io/rpc"],
+    "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.drpc.org"],
+    "opst": ["https://sepolia.optimism.io", "https://optimism-sepolia.drpc.org"],
+    "blst": ["http://testnet-rpc.blastblockchain.com"],
+    "mont": ["https://testnet-rpc.monad.xyz"],
+    "seit": ["https://evm-rpc-testnet.sei-apis.com"],
+    "bnbt": ["https://data-seed-prebsc-1-s1.bnbchain.org:8545"],
+    "unit": ["https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"]
+}'
 export PRIVATE_KEY_LOCAL="$PRIVATE_KEY"
 sleep 2
 nohup /root/executor/executor/bin/executor > executor.log 2>&1 &
